ae311a03d3c0f4d68ffcbc693af2217719386504
LazyLyra <kylesia1234@gmail.com>
2025-06-11 21:26:55 +0800

    holeh moleh 3 levels

--- NES Game Jam 2025/Assets/Scripts/KeyScript.cs
+++ NES Game Jam 2025/Assets/Scripts/KeyScript.cs
@@ -26,7 +26,7 @@ public class KeyScript : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Player")
+        if (collision.gameObject.tag == "Player" || collision.gameObject.tag == "Princess")
         {
             collected = true;
             SR.enabled = false;
