67564cba05803c623e249fe72cbf83bde5a351a2
Pandorexx <kiriku13579@gmail.com>
2025-06-15 12:44:36 +0800

    entry cutscene and music unji

--- NES Game Jam 2025/Assets/Scripts/RoomMusicPlayer.cs
+++ NES Game Jam 2025/Assets/Scripts/RoomMusicPlayer.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Build;
 using UnityEngine;
 
 public class RoomMusicPlayer : MonoBehaviour
@@ -7,13 +8,26 @@ public class RoomMusicPlayer : MonoBehaviour
     public AudioSource AS;
     [SerializeField] float rate;
     [SerializeField] float max;
+    [SerializeField] bool playing; 
     // Start is called before the first frame update
     void Start()
     {
         AS = GetComponent<AudioSource>();
         AS.volume = 0.05f;
+        if (playing)
+        {
+            DontDestroyOnLoad(gameObject);
+        }
     }
 
+    private void Awake()
+    {
+        if (!playing)
+        {
+            AS.Play();
+            playing = true;
+        }
+    }
     // Update is called once per frame
     void Update()
     {
